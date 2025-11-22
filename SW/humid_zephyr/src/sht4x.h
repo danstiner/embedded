5f46555da8821b0efc269550a3ea1e95499c4f09
Daniel Stiner <danstiner@gmail.com>
2025-11-21 23:57:53 -0800

    Still working

---
+++ SW/humid_zephyr/src/sht4x.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <lib/core/CHIPError.h>
+
+using namespace chip;
+
+class Sht4x {
+public:
+	CHIP_ERROR Init();
+
+	// Read temperature and humidity
+	// Returns true on success, false on error
+	// temperature: in 0.01°C units (Matter format)
+	// humidity: in 0.01% units (0-10000 = 0-100%)
+	// Will activate heater as necessary
+	bool Read(int16_t &temperature, uint16_t &humidity);
+
+private:
+	const struct device *sht;
+};
