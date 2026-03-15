15692a3b678ecab628b039b27b8323841acb55fa
Daniel Stiner <danstiner@gmail.com>
2026-03-15 08:56:13 -0700

    Power down RAM on matter side

--- SW/hygrometer/src/matter/main.cpp
+++ SW/hygrometer/src/matter/main.cpp
@@ -1,11 +1,19 @@
 #include "app_task.h"
 
+#if IS_ENABLED(CONFIG_RAM_POWER_DOWN_LIBRARY)
+#include <ram_pwrdn.h>
+#endif
+
 #include <zephyr/logging/log.h>
 
 LOG_MODULE_REGISTER(app, CONFIG_CHIP_APP_LOG_LEVEL);
 
 int main()
 {
+#if IS_ENABLED(CONFIG_RAM_POWER_DOWN_LIBRARY)
+	power_down_unused_ram();
+#endif
+
 	CHIP_ERROR err = AppTask::Instance().StartApp();
 
 	LOG_ERR("Exited with code %" CHIP_ERROR_FORMAT, err.Format());
